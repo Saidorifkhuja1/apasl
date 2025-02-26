93b9e38e9c14ce2339829ca1187c66570abeccd5
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-02-26 16:03:57 +0500

    apasl

--- speaker/urls.py
+++ speaker/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('speaker_detail/<uuid:uid>/', SpeakerRetrieveView.as_view()),
     path('update_speaker/<uuid:uid>/', SpeakerUpdateView.as_view()),
     path('delete_speaker/<uuid:uid>/', SpeakerDeleteView.as_view()),
-    path('speakers_list_ru/', SpeakerListRussianView.as_view()),
+    path('speaker_list_ru/', SpeakerListRussianView.as_view()),
     path('speakers_details_ru/<uuid:uid>/', SpeakerRetrieveRussianView.as_view()),
     path('speaker_list_en/', SpeakerListEnglishView.as_view()),
     path('speakers_details_en/<uuid:uid>/', SpeakerRetrieveEnglishView.as_view()),
